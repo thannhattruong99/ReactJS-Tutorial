0334a838787a65e64e5840443e8e605e47fcc126
thannhattruong99 <truongtnse130426@fpt.edu.vn>
2021-09-23 19:50:06 +0700

    Demo arrow function

--- src/User.js
+++ src/User.js
@@ -7,26 +7,16 @@ class User extends React.Component{
     constructor(props){
         super(props)
 
-        this.user =[{
-            "id": 1,
-            "name": "Than Nhat Truong 1"
-        }, {
-            "id": 2,
-            "name": "Than Nhat Truong 2"
-        }, {
-            "id": 3,
-            "name": "Than Nhat Truong 3"
-        }]
-
-        this.state = {
-            "name": ""
-        }
+        // Use it without arrow function
+        // this.hello = this.hello.bind(this)
     }
 
-    change = (event) =>{
-        this.setState({
-            "name": event.target.value
-        })
+    // wihtout arrow function
+    // hello()
+
+    // arrow function need no this.hello = this.hello.bind(this)
+    hello = (country) =>{
+        alert(`Welcome ${this.props.firstName} ${this.props.lastName} - ${country}`)
     }
 
     // <React.Fragment>
@@ -36,17 +26,7 @@ class User extends React.Component{
                 <h1>Welcome to ReactJS</h1>
                 <h2>ReactFragment Test</h2>
                 <h3>Welcome {this.props.firstName} {this.props.lastName}</h3>
-                <ul>
-                    {this.user.map(u => <li>{u.id} - {u.name}</li>)}
-                </ul>
-                <ul>
-                    {this.user.map(u => <UserInfo user={u} />)}
-                </ul>
-                <div>
-                    <input type="text" value={this.state.name} 
-                        onChange={this.change} />
-                    <h1>Welcome {this.state.name}</h1>
-                </div>
+                <input type="button" value="Click me" onClick={() => this.hello("Viet Nam")} />
             </>
         )
     }
